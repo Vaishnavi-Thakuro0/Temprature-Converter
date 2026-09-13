007f67b7124ba80ce707d146d25ab552cad00fd9
Vaishnavi-Thakuro0 <vaishnavithakur1171@gmail.com>
2026-09-13 13:54:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,11 +15,3 @@ function formatTemperature(value, unit)
   return value + " " + unit;
 }
 
-// const fahrenheit = celsiusToFahrenheit(25);
-// console.log(formatTemperature(fahrenheit, 'F'));
-// const celsius = fahrenheitToCelsius(68);
-// console.log(formatTemperature(celsius, 'C'));
-// const freezingFahrenheit = celsiusToFahrenheit(0);
-// console.log(formatTemperature(freezingFahrenheit, 'F'));
-// const freezingCelsius = fahrenheitToCelsius(32);
-// console.log(formatTemperature(freezingCelsius, 'C'));
